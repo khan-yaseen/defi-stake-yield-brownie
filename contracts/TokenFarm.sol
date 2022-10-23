a7517a14d4566449de126fe17c3c7a36182a57ac
yaseen khan <yaseenkhan61@yahoo.com>
2022-10-23 18:41:06 +0530

    added a way to issueToken(rewards) and unstake tokens

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -4,24 +4,79 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract TokenFarm is Ownable {
 
-// stakeTokens
-// unStakeTokens
-// issueTokens
-// addAllowedTokens
-// getEthValue
+// stakeTokens - DONE!
+// unStakeTokens - DONE!
+// issueTokens - DONE!
+// addAllowedTokens - DONE!
+// getValue - DONE!
 
     // mapping token address -> staker address -> amount
     mapping(address => mapping(address => uint256)) public stakingBalance;
     mapping(address => uint256) public uniqueTokensStaked;
+    mapping(address => address) public tokenPriceFeedMapping;
     address[] public stakers;
     address[] public allowedTokens;
     IERC20 public dappToken;
 
-    constructor(address _dappTokenAddress) public {
+    constructor(address _dappTokenAddress) {
         dappToken = IERC20(_dappTokenAddress) ;
+    }
+
+    function setPriceFeedContract(address _token, address _priceFeed) public onlyOwner {
+        tokenPriceFeedMapping[_token] = _priceFeed;
+    }
+
+    // 100 ETH 1:1 for every 1ETH, we give 1 DappToken
+    // 50 ETH and 50 DAI staked, and we want to give a reward of 1 DAPP / 1 DAI
+
+    function issueTokens() public onlyOwner {
+        // Issue tokens to all stakers
+        for( uint256 stakersIndex = 0; stakersIndex < stakers.length; stakersIndex++) {
+            address recipient = stakers[stakersIndex];
+            // send them a token reward 
+            // based on their total value locked
+            // dappToken.transfer(recepient, ???)
+            uint256 userTotalValue = getUserTotalValue(recipient);
+            dappToken.transfer(recipient, userTotalValue);
+        }
+    }
+
+    function getUserTotalValue(address _user) public view returns (uint256){
+        uint256 totalValue = 0;
+        require(uniqueTokensStaked[_user] > 0, "No tokens staked!");
+        for ( uint256 allowedTokensIndex = 0; allowedTokensIndex < allowedTokens.length; allowedTokensIndex++) {
+            totalValue = totalValue + getUserSingleTokenValue(_user, allowedTokens[allowedTokensIndex]);
+        }
+        return totalValue;
+    }
+
+    function getUserSingleTokenValue(address _user, address _token) public view returns (uint256) {
+        // 1 ETH -> $1300
+        // 1300
+        // 200 DAI -> $200
+        // 200
+        if (uniqueTokensStaked[_user] < 0) {
+            return 0;
+        }
+        // price of the token * stakingBalance[_token][_user]
+        (uint256 price, uint256 decimals) = getTokenValue(_token);
+        return 
+        // 1000000000000000000 ETH
+        // ETH/USD -> 100000000
+        // 10 * 100 = 1000
+        (stakingBalance[_token][_user] * price / (10**decimals));
+    }
+
+    function getTokenValue(address _token) public view returns (uint256, uint256) {
+        address priceFeedAddress = tokenPriceFeedMapping[_token];
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        uint256 decimals = uint256(priceFeed.decimals());
+        return (uint256(price), decimals);
     }
 
     function stakeTokens(uint256 _amount, address _token) public {
@@ -39,9 +94,18 @@ contract TokenFarm is Ownable {
         }
     }
 
-    updateUniqueTokensStaked(address _user, address _token) internal {
+    function unstakeTokens(address _token)  public{
+        uint256 balance = stakingBalance[_token][msg.sender];
+        require(balance > 0, "Staking balance cannot be 0");
+        IERC20(_token).transfer(msg.sender, balance);
+        stakingBalance[_token][msg.sender] = 0;
+        uniqueTokensStaked[msg.sender] = uniqueTokensStaked[msg.sender] - 1;
+        // TODO : Write functionality to update the stakers list
+    }
+
+    function updateUniqueTokensStaked(address _user, address _token) internal {
         if(stakingBalance[_token][_user] <= 0) {
-            uniqueTokensStaked[_user] = uniqueTokensStaked + 1;
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] - 1;
         }
     }
 
