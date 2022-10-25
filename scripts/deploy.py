556fd7fd1dd8eb8b97f5f0c90729b9e4f62c1a84
yaseen khan <yaseenkhan61@yahoo.com>
2022-10-25 19:56:42 +0530

    update frontend with brownie-config and build artifacts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,14 @@
 from web3 import Web3
 from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, network, config
+import yaml
+import json
+import os
+import shutil
 
 KEPT_BALANCE = Web3.toWei(100, "ether")
 
-def deploy_token_farm_and_dapp_token():
+def deploy_token_farm_and_dapp_token(front_end_update=False):
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
@@ -25,8 +29,9 @@ def deploy_token_farm_and_dapp_token():
         fau_token: get_contract("dai_usd_price_feed"),
         weth_token: get_contract("eth_usd_price_feed")
     }
-
     add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    if front_end_update:
+        front_end_update()
     return token_farm, dapp_token
 
 def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
@@ -37,5 +42,20 @@ def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
         set_tx.wait(1)
     return token_farm 
 
+def update_front_end():
+    # Send the build folder
+    copy_folders_to_fron_end("./build", "./front_end/src/chain-info")
+    # Sending the fron end our config in JSON format
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated!")
+
+def copy_folders_to_fron_end(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
+
 def main():
-    deploy_token_farm_and_dapp_token()
+    deploy_token_farm_and_dapp_token(front_end_update=True)
