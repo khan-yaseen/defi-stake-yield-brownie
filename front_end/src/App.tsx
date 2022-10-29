1c382e0ac1709d64c40317ebd80cf284d31cd4c0
yaseen khan <yaseenkhan61@yahoo.com>
2022-10-29 09:07:13 +0530

    Added Main component

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { DAppProvider, Goerli, Mainnet } from "@usedapp/core";
-import {Header} from "./components/Header"
-import {Container} from "@material-ui/core"
+import { DAppProvider, Goerli } from "@usedapp/core";
+import { Header } from "./components/Header";
+import { Container } from "@material-ui/core";
+import { Main } from "./components/Main";
 
 function App() {
   return (
     <DAppProvider
       config={{
-        networks: [Goerli],
+        networks: [Goerli]
       }}
     >
-      <Header/>
+      <Header />
       <Container maxWidth="md">
-      <div>Hi!</div>
+        <Main />
       </Container>
     </DAppProvider>
   );
